b07ac2cbe04bc724f6aa7227eda585a955b37f9a
Mao Kobayashi <kobamao.jp@gmail.com>
2024-01-23 13:42:56 +0000

    version 1.0.3

--- R/DelLessData.R
+++ R/DelLessData.R
@@ -1,12 +1,47 @@
+#' Removes groups with identical missing data patterns having at most a given number of cases
+#' @rdname DelLessData
+#' @aliases DelLessData
+#' 
+#' @description
+#' Removes groups of missing data patterns with number of cases 
+#' less than or equal to a specified value (ncases).
+#' 
+#' @param data A matrix consisting of at least two columns. Values must be numerical with missing data indicated by NA.
+#' @param ncases Missing data pattern groups with ncases number of cases or less will be removed from the data set.
+#' 
+#' @returns
+#' \item{data }{A matrix of reaaranged data, according to missing data patterns, with missing data patterns having less than ncases number of cases removed.}
+#' \item{patused }{A matrix indicating the missing data patterns in the data set; observed variable(s) are indiated by  1's' and missing variables are indicated by NA's.}
+#' \item{patcnt}{A vector consisting of the number of cases corresponding to each pattern in patused.}
+#' \item{spatcnt}{Cumulative sum of elements of patcnt.}
+#' \item{g}{Number of missing data patterns.}
+#' \item{caseorder}{A mapping of case number indices from output data (rearranged data) to input data.}
+#' \item{removedcases }{The index of cases that were removed from the original data set}
+#' 
+#' @author 
+#' Mortaza Jamshidian, Siavash Jalal, and Camden Jansen
+#' 
+#' @examples
+#' set.seed <- 50
+#' n <- 200
+#' p <- 4
+#' pctmiss <- 0.2
+#' y <- matrix(rnorm(n * p),nrow = n)
+#' missing <- matrix(runif(n * p), nrow = n) < pctmiss
+#' y[missing] <- NA
+#' out <- DelLessData(data=y, ncases = 4)
+#' dim(y)
+#' dim(out$data)
+#' 
+#' @export
 DelLessData <- function(data, ncases = 0) {
-#This function deletes cases of a missing pattern with less than or equal to ncases 
+  # This function deletes cases of a missing pattern with less than or equal to ncases 
   if(length(data)==0)
- {
-   cat("Warning: data is empty")
-   return
- }
+  {
+    stop("data is empty")
+  }
   if (is.matrix(data)) {
-  data <- OrderMissing(data)
+    data <- OrderMissing(data)
   }
   n <- nrow(data$data)
   p <- ncol(data$data)
@@ -15,7 +50,7 @@ DelLessData <- function(data, ncases = 0) {
   rm <- c()
   removedcases <- c()
   if(length(ind) != 0){
-    #cat("cases with insufficient number of observations were removed")
+    # cat("cases with insufficient number of observations were removed")
     for(i in 1:length(ind))
     {
       rm <- c(rm, seq(spatcntz[ind[i]] + 1, spatcntz[ind[i] + 1]));
@@ -27,17 +62,17 @@ DelLessData <- function(data, ncases = 0) {
     caseorder <- data$caseorder[-1 * rm]
     spatcnt <- cumsum(patcnt)
   }else {
-  patused <- data$patused
-  patcnt <- data$patcnt
-  spatcnt <- data$spatcnt
-  caseorder <- data$caseorder
-  y <- data$data
+    patused <- data$patused
+    patcnt <- data$patcnt
+    spatcnt <- data$spatcnt
+    caseorder <- data$caseorder
+    y <- data$data
   }
   
   newdata <- list(data = y, patused = patused, patcnt = patcnt,
                   spatcnt = spatcnt, g = length(patcnt), caseorder = caseorder, 
                   removedcases = removedcases)
-#  colnames(newdata)<-colnames(data)
+  # colnames(newdata)<-colnames(data)
   class(newdata) <- "orderpattern"
   newdata
 }
--- R/OrderMissing.R
+++ R/OrderMissing.R
@@ -1,20 +1,65 @@
+#' Order Missing Data Pattern
+#' @rdname OrderMissing
+#' @aliases OrderMissing print.orderpattern summary.orderpattern
+#' 
+#' @description
+#' This function rearranges the data based on their missing data patterns. 
+#' Morever, missing data patterns consisting of fewer than the user specified number in del.lesscases is deleted from the dataset.
+#' 
+#' @param data A matrix or data frame consisting of at least two columns. Values must be numerical with missing data indicated by NA.
+#' @param del.lesscases Missing data patterns consisting of del.lesscases number of cases or less will be removed from the data set.
+#' 
+#' @returns 
+#' An object of class \code{orderpattern} - a list including elements:
+#' \item{data }{A matrix containing the rearranged data set}
+#' \item{caseorder}{A mapping of case number indices from output (ordered) data to input data. More specifically, the j-th row of the output (ordered) 
+#'   data is the caseorder[j]-th (the j-th element of caseorder) row of the input data.}
+#' \item{patused }{A matrix indicating the missing data patterns in the data set, using 1's' (for observed) and NA's (for missing)}
+#' \item{patcnt}{A vector consisting the number of cases corresponding to each pattern in patused}
+#' \item{spatcnt}{Cumulative sum of elements of patcnt}
+#' \item{g}{Number of missing data patterns}
+#' \item{removedcases }{The index of cases that were removed from the original data set}
+#' 
+#' @author 
+#' Mortaza Jamshidian, Siavash Jalal, and Camden Jansen
+#' 
+#' @note
+#' If you run the following command line:
+#' out <- OrderMissing(data=y, del.lesscases=0),
+#' then y[out$caseorder,] is equal to the output data (i.e. out$data). 
+#' Also out$data[order(out$caseorder),] is equal to the original data y. Note that
+#' if del.lesscases is greater than zero, the command out$data[order(out$caseorder),] will result in
+#' a data set that has no case order correspondnce to the original data.
+#' 
+#' @examples
+#' set.seed <- 50
+#' n <- 200
+#' p <- 4
+#' pctmiss <- 0.2
+#' y <- matrix(rnorm(n * p),nrow = n)
+#' missing <- matrix(runif(n * p), nrow = n) < pctmiss
+#' y[missing] <- NA
+#' out <- OrderMissing(y, del.lesscases = 0)
+#' a <- out$caseorder
+#' z = out$data
+#' y[a,] # Reverting the original data to the new output order
+#' z[order(a),] # Reverting the ordered datat to the original order
+#' @export
 OrderMissing <- function(data, del.lesscases = 0)
 {
-# case order has the order of the original data
+  # case order has the order of the original data
   y <- data
   if (is.data.frame(y)) {
-  y <- as.matrix(y)
+    y <- as.matrix(y)
   } 
   if(!is.matrix(y))
   {
-    cat("Warning data is not a matrix or data frame")
-    stop("")
+    stop("data is not a matrix or data frame")
   }
   if(length(y)==0)
- {
-   cat("Warning: data is empty")
-   return
- }
+  {
+    stop("data is empty")
+  }
   names <- colnames(y)
   n <- nrow(y)
   pp <- ncol(y)
@@ -74,8 +119,8 @@ OrderMissing <- function(data, del.lesscases = 0)
   colnames(patused) <- names
   spatcnt <- cumsum(patcnt)
   dataorder <- list(data = yfinal, patused = patused, patcnt = patcnt, 
-                   spatcnt = spatcnt, g = length(patcnt), 
-                   caseorder = caseorder, removedcases = removedcases)
+                    spatcnt = spatcnt, g = length(patcnt), 
+                    caseorder = caseorder, removedcases = removedcases)
   dataorder$call <- match.call()
   class(dataorder) <- "orderpattern"
   if(del.lesscases > 0)
@@ -93,18 +138,27 @@ OrderMissing <- function(data, del.lesscases = 0)
 # class(temp) <- "ordered"
 # temp
 #}
+
+#' @rdname OrderMissing
+#' @aliases OrderMissing print.orderpattern summary.orderpattern
+#' @usage NULL
+#' @export
 print.orderpattern <- function(x, ...) {
- cat("Call:\n")
- print(x$call)
- cat("\nNumber of Ptterns: ", x$g, "\n")
- cat("\nPttern used:\n")
- ni <- x$patcnt
- disp.patt <- cbind(x$patused, ni)
- colnames(disp.patt)[ncol(disp.patt)] <- "Number of cases"
- rownames(disp.patt) <- rownames(disp.patt, do.NULL = FALSE, prefix = "group.")
- print(disp.patt, print.gap = 3) 
+  cat("Call:\n")
+  print(x$call)
+  cat("\nNumber of Ptterns: ", x$g, "\n")
+  cat("\nPttern used:\n")
+  ni <- x$patcnt
+  disp.patt <- cbind(x$patused, ni)
+  colnames(disp.patt)[ncol(disp.patt)] <- "Number of cases"
+  rownames(disp.patt) <- rownames(disp.patt, do.NULL = FALSE, prefix = "group.")
+  print(disp.patt, print.gap = 3) 
 }
 
+#' @rdname OrderMissing
+#' @aliases OrderMissing print.orderpattern summary.orderpattern
+#' @usage NULL
+#' @export
 summary.orderpattern <- function(object, ...) {
   summary(object$data)
 }
